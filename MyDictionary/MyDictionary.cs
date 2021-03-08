ecc1a585ddcf5777a4ec704d5d039eab815437f7
edacoskunn <edaaacoskunn5@gmail.com>
2021-03-08 22:54:36 +0300

    first commit

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] Keys;
+        V[] Values;
+
+        public MyDictionary()
+        {
+            Keys = new K[0];
+            Values = new V[0];
+        }
+        public void Add(K anahtar, V deger)
+        {
+            V[] Tempvalues = Values;
+            K[] Tempkeys = Keys;
+            Keys = new K[Keys.Length + 1];
+            Values = new V[Values.Length + 1];
+            for (int i = 0; i < Tempvalues.Length; i++)
+            {
+                Keys[i] = Tempkeys[i];
+                Values[i] = Tempvalues[i];
+            }
+            Keys[Keys.Length - 1] = anahtar;
+            Values[Keys.Length - 1] = deger;
+        }
+        public void List()
+        {
+            for (int i = 0; i < Keys.Length; i++)
+            {
+                Console.WriteLine("[" + Keys[i] + "," + Values[i] + "]");
+            }
+        }
+        public int Count
+        {
+            get { return Keys.Length; }
+        }
+        public K[] OnlyGetKeys
+        {
+            get { return Keys; }
+        }
+        public V[] OnlyGetValues
+        {
+            get { return Values; }
+        }
+        public bool ContaninsKeys( K keys)
+        {
+            bool deger = true;
+            if (Keys.Contains(keys)==true)
+            {
+                deger = true;
+            }
+            else
+            {
+                deger = false;
+            }
+            return deger;
+        }
+        public bool ContaninsValues(V values)
+        {
+            bool deger = true;
+            if (Values.Contains(values) == true)
+            {
+                deger = true;
+            }
+            else
+            {
+                deger = false;
+            }
+            return deger;
+        }
+        public bool Remove(K keys)
+        {
+            bool deger = true;
+            if (Keys.Contains(keys) == true)
+            {
+               int yenideger= Array.IndexOf(Keys, keys);
+                Console.WriteLine("silindi");
+                Array.Clear(Keys, yenideger,1);
+                Array.Clear(Values, yenideger, 1);
+                
+                deger = true;
+            }
+            else
+            {
+                deger = false;
+            }
+            return deger;
+        }
+        public void Clear(K keys,int eleman)
+        {
+            int yenikeys = Convert.ToInt32(keys);
+            Array.Clear(Keys, yenikeys, eleman);
+            Array.Clear(Values, yenikeys, eleman);
+        }
+        public void KeysList()
+        {
+            Console.Write("[");
+            for (int i = 0; i < Keys.Length - 1; i++)
+            {
+                Console.Write(Keys[i] + ",");
+            }
+            Console.WriteLine(Keys[Keys.Length - 1] + "]");
+        }
+        public void ValuesList()
+        {
+            Console.Write("[");
+            for (int i = 0; i < Values.Length - 1; i++)
+            {
+                Console.Write(Values[i] + ",");
+            }
+            Console.WriteLine(Keys[Values.Length - 1] + "]");
+        }
+        public bool TryGetValue(K keys, out V value)
+        {
+            
+            if (Keys.Contains(keys) == true)
+            {
+                int yenideger = Array.IndexOf(Keys, keys);
+                value = Values[yenideger];
+                Console.WriteLine(value);
+                return true;
+            }
+            value = default(V);
+                 return false;
+        }  
+        }
+    }
+
+
+
+
+
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(2, "elma");
+            myDictionary.Add(3, "portakal");
+            myDictionary.Add(5, "mandalina");
+            myDictionary.Add(7, "nar");
+            myDictionary.List();
+            MyDictionary<int, char> kalemList = new MyDictionary<int, char>();
+            kalemList.Add(1, 'a');
+            kalemList.Add(2, 'b');
+            kalemList.Add(3, 'c');
+            kalemList.Add(4, 'd');
+            kalemList.List();
+            Console.WriteLine(myDictionary.Count);
+            foreach (var item in kalemList.OnlyGetKeys)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(kalemList.ContaninsKeys(2));
+            Console.WriteLine(kalemList.ContaninsValues('e'));
+            kalemList.Remove(3);
+            kalemList.List();
+            Console.WriteLine();
+            kalemList.Clear(1, 1);
+            kalemList.List();
+            Console.WriteLine(kalemList.Count);
+            kalemList.KeysList();
+            kalemList.ValuesList();
+           bool ValueBul= kalemList.TryGetValue(1,out char c);
+            Console.WriteLine(ValueBul);
+        }
+    }
+    }
+
+
+
+
+
+
